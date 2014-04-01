38d998479cbc0b252228899cc101ac551c10337a
Nahuel Chaves <nahuel.chaves@gmail.com>
2014-04-01 00:22:34 -0300

    Permissions, errors, crud

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -6,13 +6,17 @@ Template.postSubmit.events({
             url: $(e.target).find('[name=url]').val(),
             title: $(e.target).find('[name=title]').val(),
             message: $(e.target).find('[name=message]').val()
-        }
+        };
+
         Meteor.call('post', post, function(error, id){
-            if (error)
-                return alert(error.reason);
-
-            Router.go('postPage', {_id: id});
+            if (error) {
+                // display the error to the user
+                throwError(error.reason);
+                if (error.error === 302)
+                    Router.go('postPage', {_id: error.details})
+            } else {
+                Router.go('postPage', {_id: id});
+            }
         });
-
     }
 });
--- collections/posts.js
+++ collections/posts.js
@@ -1,4 +1,14 @@
 Posts = new Meteor.Collection('posts');
+Posts.allow({
+    update: ownsDocument,
+    remove: ownsDocument
+});
+
+Posts.deny({
+    update: function(userId, post, fieldNames){
+        return (_.without(fieldNames, 'url','title').length > 0);
+    }
+});
 Meteor.methods({
     post: function(postAttributes) {
         var user = Meteor.user(),
@@ -15,19 +25,29 @@ Meteor.methods({
         // check that there are no previous posts with the same link
         if (postAttributes.url && postWithSameLink) {
             throw new Meteor.Error(302,
-                'This link has already been posted',
+                'El link ya existe',
                 postWithSameLink._id);
         }
 
         // pick out the whitelisted keys
-        var post = _.extend(_.pick(postAttributes, 'url', 'title', 'message'), {
+        var post = _.extend(_.pick(postAttributes, 'url', 'message'), {
+            title: postAttributes.title + (this.isSimulation ? '(client)' : '(server)'),
             userId: user._id,
             author: user.username,
             submitted: new Date().getTime()
         });
 
-        var postId = Posts.insert(post);
+        // wait for 5 seconds
+        if (! this.isSimulation) {
+            var Future = Npm.require('fibers/future');
+            var future = new Future();
+            Meteor.setTimeout(function() {
+                future.return();
+            }, 5 * 1000);
+            future.wait();
+        }
 
+        var postId = Posts.insert(post);
         return postId;
     }
 });
